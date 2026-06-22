cc03d91d9da086f8c739b6d4f36c649e32c996b7
= <dev.miteshgodne@gmail.com>
2026-06-22 14:20:51 +0530

    Registering Regulation to Admin panel

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from tournaments.models import Regulation
+@admin.register(Regulation)
 
-# Register your models here.
+class RegulationAdmin(admin.ModelAdmin):
+    list_display = ['regulation_id', 'tournament_format','match_format', 'innings_per_match', 'overs_per_innings',  'points_for_win']
+    list_filter = ['tournament_format', 'match_format']
