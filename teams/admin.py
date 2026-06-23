2d34326c6c3c1137cf308390779cc6724de96de7
= <dev.miteshgodne@gmail.com>
2026-06-23 11:17:29 +0530

    Registering teams model to the admin panel

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Team
 
-# Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['team_name', 'short_name', 'team_head', 'coach_name', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['team_name', 'short_name', 'team_head__email']
+    ordering = ['-created_at']
+    readonly_fields = ['team_id', 'created_at', 'updated_at']
