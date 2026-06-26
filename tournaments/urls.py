6f8bdac097d4ac1d7dd1be185f7bd68b2c240c1c
= <dev.miteshgodne@gmail.com>
2026-06-27 02:52:34 +0530

    Creating & Registering TournamentOrganizer ModelViewSet

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import RegulationViewSet, TournamentViewSet, ApplicationViewSet, GroupViewSet, TournamentStandingViewSet
+from .views import RegulationViewSet, TournamentViewSet, ApplicationViewSet, GroupViewSet, TournamentStandingViewSet, TournamentOrganizerViewSet
 
 router = DefaultRouter()
 router.register('regulations', RegulationViewSet, basename='regulation')
 router.register('tournaments', TournamentViewSet, basename='tournament')
 router.register('applications', ApplicationViewSet, basename='application')
+router.register('organizers', TournamentOrganizerViewSet, basename='organizer')
 router.register('groups', GroupViewSet, basename='groups')
 router.register('standings', TournamentStandingViewSet, basename='standing')
 
--- tournaments/views.py
+++ tournaments/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .models import Regulation, Tournament, Application, Group, TournamentStanding
-from .serializers import RegulationSerializer, TournamentSerializer, ApplicationSerializer, GroupSerializer, TournamentStandingSerializer
+from .models import Regulation, Tournament, Application, Group, TournamentStanding, TournamentOrganizer
+from .serializers import RegulationSerializer, TournamentSerializer, ApplicationSerializer, GroupSerializer, TournamentStandingSerializer, TournamentOrganizerSerializer
 
 class RegulationViewSet(viewsets.ModelViewSet):
     queryset = Regulation.objects.all()
@@ -9,6 +9,10 @@ class RegulationViewSet(viewsets.ModelViewSet):
 class TournamentViewSet(viewsets.ModelViewSet):
     queryset = Tournament.objects.select_related('regulation', 'created_by').all()
     serializer_class = TournamentSerializer
+
+class TournamentOrganizerViewSet(viewsets.ModelViewSet):
+    queryset = TournamentOrganizer.objects.select_related('tournament', 'user').all()
+    serializer_class = TournamentOrganizerSerializer
 
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.select_related('team', 'tournament').all()
