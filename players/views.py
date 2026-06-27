5988cfe8e29220b06565d56c9cd381bcfb335c64
= <dev.miteshgodne@gmail.com>
2026-06-27 13:11:15 +0530

    Adding filter in players model to view only related players

--- players/views.py
+++ players/views.py
@@ -5,3 +5,4 @@ from .serializers import PlayerSerializer
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.select_related('current_team').all()
     serializer_class = PlayerSerializer
+    filterset_fields = ['current_team', 'is_active']
