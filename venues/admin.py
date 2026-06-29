a8ab6f21aa261bbaf802bd362d85dae884982ea4
= <dev.miteshgodne@gmail.com>
2026-06-29 17:52:07 +0530

    Adding created_by field in venues to facilitate authentication& permissions

--- venues/admin.py
+++ venues/admin.py
@@ -3,6 +3,6 @@ from venues.models import Venue
 
 @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
-    list_display = ['name', 'city', 'state', 'country']
-    search_fields = ['name', 'city']
+    list_display = ['name', 'city', 'state', 'country', 'created_by']
+    search_fields = ['name', 'city', 'created_by']
     list_filter = ['state', 'country']
--- venues/views.py
+++ venues/views.py
@@ -1,9 +1,12 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Venue
 from .serializers import VenueSerializer
+from core.permissions import ReadOnly, IsOrganizer
 
 class VenueViewSet(viewsets.ModelViewSet):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
-    permission_classes = [permissions.AllowAny]
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
 
